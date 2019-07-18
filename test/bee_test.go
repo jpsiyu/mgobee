8e84aeba5cbec879df6921b0d0220164a724a428
jpsiyu <jpsiyu@163.com>
2019-07-18 11:30:26 +0800

    unit test

--- test/bee_test.go
+++ test/bee_test.go
@@ -18,9 +18,23 @@ func TestConnect(t *testing.T) {
 	}
 }
 
-func TextPing(t *testing.T) {
+func TestPing(t *testing.T) {
 	bee := mgobee.Create("mydb", "me", "123", []string{})
-	err := bee.Ping()
+	err := bee.Connect("mongodb://localhost:27017")
+	if err != nil {
+		t.Error(err)
+	}
+	err = bee.Ping()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSmartConnect(t *testing.T) {
+	bee := mgobee.Create("mydb", "me", "123", []string{"mongodb://mongo:27017", "mongodb://localhost:27017"})
+	dbchan := make(chan error)
+	go bee.SmartConnect(dbchan)
+	err := <-dbchan
 	if err != nil {
 		t.Error(err)
 	}
